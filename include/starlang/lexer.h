521d31c3fcc4c9f880326b1b4c36afbc2fc17742
alperenozdnc <alperen@ozdinc.org>
2026-01-05 23:33:55 +0300

    chore(lexer): define some lexeme types

---
+++ include/starlang/lexer.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <stddef.h>
+
+/*
+ * the enum that lists all lexeme types.
+ */
+typedef enum {
+    LEX_EOF,
+    LEX_SEMI,
+    LEX_COMMENT,
+    LEX_IMPORT,
+    LEX_PERIOD,
+
+    LEX_FOR,
+    LEX_IF,
+    LEX_RET,
+    LEX_WHILE,
+    LEX_WHEN,
+    LEX_DECLARE,
+    LEX_NOTHING,
+
+    LEX_LPAREN,
+    LEX_RPAREN,
+    LEX_LBRACKET,
+    LEX_RBRACKET,
+    LEX_LSQUIRLY,
+    LEX_RSQUIRLY,
+
+    LEX_DQUOTE,
+    LEX_QUOTE,
+
+    LEX_EQ,
+    LEX_PLUS,
+    LEX_MINUS,
+    LEX_SLASH,
+    LEX_MULTIPLY,
+    LEX_LESS_THAN,
+    LEX_GREATER_THAN,
+
+    LEX_IDENT,
+    LEX_NUM,
+    LEX_STRING,
+    LEX_TRUE,
+    LEX_FALSE
+} lexeme_type_t;
+
+typedef struct {
+    lexeme_type_t kind;
+
+    char *val_start;
+    size_t val_size;
+
+    size_t line;
+    size_t col;
+} lexeme_t;
