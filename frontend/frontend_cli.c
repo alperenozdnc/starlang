765214ee1f7642ceb5ebd21ff7eb831d4370ad2c
alperenozdnc <alperen@ozdinc.org>
2026-01-06 21:46:40 +0300

    feat(frontend): cli frontend

---
+++ frontend/frontend_cli.c
@@ -0,0 +1,54 @@
+#include <starlang/def.h>
+#include <starlang/frontend.h>
+#include <starlang/utils.h>
+
+#include <ezcli.h> // my project too, see in github
+
+ret_e main_opt_body(CLI_IGNORE_CTX, char *pathname) {
+    if (!util_does_path_exist(pathname)) {
+        FRONTEND_PRINT(FRONTEND_ERR, FRONTEND_PRINT_PREFIX,
+                       "path '%s' doesn't exist on disk.", pathname);
+
+        return RET_FAIL;
+    }
+
+    if (!util_is_path_a_file(pathname)) {
+        FRONTEND_PRINT(FRONTEND_ERR, FRONTEND_PRINT_PREFIX,
+                       "path '%s' isn't a file.", pathname);
+
+        return RET_FAIL;
+    }
+
+    if (!util_validate_file_ext(pathname, STARLANG_FILE_EXT)) {
+        FRONTEND_PRINT(FRONTEND_WARN, FRONTEND_PRINT_PREFIX,
+                       "starlang files should have '%s' as their "
+                       "extension for clarity.",
+                       STARLANG_FILE_EXT);
+    }
+
+    if (frontend_entrypoint(pathname))
+        return RET_NORMAL;
+
+    FRONTEND_PRINT(FRONTEND_ERR, FRONTEND_PRINT_PREFIX,
+                   "failed to run '%s'. exiting.", pathname);
+
+    return RET_FAIL;
+}
+
+opt_s main_opt = {
+    .aliases = CLI_ALIASES(CLI_NONOPT),
+    .body = main_opt_body,
+};
+
+void frontend_cli(int argc, char *argv[]) {
+    CLI_ALLOW_NONOPT = true;
+    cli_s cli = {0};
+    opt_s *opts[] = {&main_opt, NULL};
+
+    initcli(&cli, CLI_CMD_NAME, CLI_DESC, CLI_USAGE, CLI_FOOTER, opts,
+            CLI_HELP_ALIASES);
+
+    runcli(&cli, argc, argv);
+
+    freecli(&cli);
+}
---
+++ include/starlang/frontend.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <ezcli.h>
+
+// yes this does use cliprint which is an ezcli primitive but it's definitely
+// better to have a unified printer
+#define FRONTEND_ERR CLI_ERROR
+#define FRONTEND_WARN CLI_WARN
+#define FRONTEND_HINT CLI_HINT
+#define FRONTEND_PRINT_PREFIX "[starlang] "
+
+#define FRONTEND_PRINT(code, prefix, msg, ...)                                 \
+    cliprint(code, prefix, msg, __VA_ARGS__)
+
+/*
+ * interprets starlang code in given filename. returns boolean based on success.
+ */
+bool frontend_entrypoint(char *filename);
+
+#define CLI_CMD_NAME "star"
+
+#define CLI_DESC "an interpreted language made for educational purposes."
+
+#define CLI_USAGE "{filename.st}"
+
+#define CLI_FOOTER                                                             \
+    "project license: "                                                        \
+    "<https://www.gnu.org/licenses/gpl-3.0.en.html>.\nsource code: "           \
+    "<https://github.com/alperenozdnc/starlang>\n"
+
+#define CLI_HELP_ALIASES CLI_ALIASES("help", "--help")
+
+/*
+ * initializes a cli instance.
+ */
+void frontend_cli(int argc, char *argv[]);
