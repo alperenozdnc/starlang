61a3878f660bb76bbc3a926afa46d78d63e234b6
alperenozdnc <alperen@ozdinc.org>
2026-01-07 20:47:21 +0300

    fix(utils): `util_read_file` now actually returns null-termed strings

--- utils/util_read_file.c
+++ utils/util_read_file.c
@@ -3,10 +3,13 @@
 #include <starlang/utils.h>
 
 #include <stdio.h>
+#include <string.h>
 
 char *util_read_file_into_arena(arena_t *arena, FILE *f) {
     ssize_t file_size = util_get_file_size(f);
+
     char *buf = arena_alloc(arena, (size_t)file_size);
+
     ssize_t read_size = fread(buf, 1, (size_t)file_size, f);
 
     if (read_size != file_size) {
@@ -14,6 +17,8 @@ char *util_read_file_into_arena(arena_t *arena, FILE *f) {
 
         return NULL;
     }
+
+    buf[file_size] = '\0';
 
     return buf;
 }
