daffe732ce09c2d611a306e20f777a05f82c7736
alperenozdnc <alperen@ozdinc.org>
2026-01-07 20:52:19 +0300

    chore(frontend): frontend just prints the file for now

--- frontend/frontend_entrypoint.c
+++ frontend/frontend_entrypoint.c
@@ -1,9 +1,20 @@
+#include <starlang/arena.h>
 #include <starlang/frontend.h>
+#include <starlang/utils.h>
 
 #include <stdio.h>
 
 bool frontend_entrypoint(char *filename) {
-    printf("assume this runs '%s'.\n", filename);
+    FILE *file = fopen(filename, "r");
+    size_t file_size = util_get_file_size(file);
+
+    arena_t *frontend_arena = arena_init(file_size);
+    char *buf = util_read_file_into_arena(frontend_arena, file);
+
+    printf("%s", buf);
+
+    arena_free(frontend_arena);
+    fclose(file);
 
     return true;
 }
