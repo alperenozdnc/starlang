40b7e14b3e6c878f819fadda55b950aba9dbfa79
alperenozdnc <alperen@ozdinc.org>
2026-01-12 01:11:26 +0300

    refactor(utils): `str_ends_with_suffix` now takes const strings

--- utils/util_check_str_ends_with_suffix.c
+++ utils/util_check_str_ends_with_suffix.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool util_does_str_end_with_suffix(char *str, char *suffix) {
+bool util_does_str_end_with_suffix(const char *str, char *suffix) {
     char *result = strstr(str, suffix);
 
     return (result && result == str + strlen(str) - strlen(suffix));
