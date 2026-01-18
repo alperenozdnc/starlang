742109bc6badb351e8dc7f9607d83ae03022ab61
alperenozdnc <alperen@ozdinc.org>
2026-01-18 21:17:15 +0300

    feat(replacer): `print_flattened_gnt` function

---
+++ replacer/replacer_print_flattened_gnt.c
@@ -0,0 +1,19 @@
+#include <starlang/replacer.h>
+#include <stdio.h>
+
+void replacer_debug_print_flat_gnt(nmspc_link_t **gnt_flat,
+                                   size_t link_count) {
+    printf("{ ");
+
+    for (size_t i = 0; i < link_count; i++) {
+        nmspc_node_t *node = gnt_flat[i]->self;
+
+        printf("%s>%s", node->namespace, node->module);
+
+        if (i + 1 < link_count) {
+            printf(", ");
+        }
+    }
+
+    printf(" }\n");
+}
