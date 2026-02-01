86693712b91946a0ebefe6ce9c6635fad2ca3ce0
alperenozdnc <alperen@ozdinc.org>
2026-02-01 16:07:59 +0300

    chore(frontend): call the new lexer function

--- frontend/frontend_entrypoint.c
+++ frontend/frontend_entrypoint.c
@@ -1,27 +1,26 @@
 #include <starlang/arena.h>
 #include <starlang/frontend.h>
-#include <starlang/replacer.h>
 #include <starlang/utils.h>
 
-#include <stdio.h>
+#include <starlang/lexer.h>
+#include <starlang/replacer.h>
 
 bool frontend_entrypoint(char *rel_main_module_path, char *parent_path,
                          char *filename) {
     FILE *file = fopen(rel_main_module_path, "r");
     size_t file_size = util_get_file_size(file);
 
-    arena_t *intermediate_arena = arena_init(file_size);
-    char *buf = util_read_file_into_arena(intermediate_arena, file);
+    arena_t *trans_arena = arena_init(file_size);
+    char *buf = util_read_file_into_arena(trans_arena, file);
 
-    char *main_module_path =
-        util_build_str(intermediate_arena, parent_path, filename);
+    char *main_module_path = util_build_str(trans_arena, parent_path, filename);
 
-    src_t *source = replacer(intermediate_arena, main_module_path, parent_path,
+    src_t *source = replacer(trans_arena, main_module_path, parent_path,
                              filename, buf, file_size - 1);
 
-    (void)source;
+    lexer(trans_arena, source);
 
-    arena_free(intermediate_arena);
+    arena_free(trans_arena);
     fclose(file);
 
     return true;
