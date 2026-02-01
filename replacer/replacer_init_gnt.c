d2deab2ac5ebfc97802d3d0f9f8e6f77a8f8293a
alperenozdnc <alperen@ozdinc.org>
2026-02-01 03:06:13 +0300

    chore(replacer): GNT init now adds content info too

--- replacer/replacer_init_gnt.c
+++ replacer/replacer_init_gnt.c
@@ -4,6 +4,12 @@
 
 #include <string.h>
 
-nmspc_link_t *replacer_init_gnt(arena_t *arena, char *path, char *module) {
-    return replacer_init_nmspc_link(arena, NULL, path, "", module);
+nmspc_link_t *replacer_init_gnt(arena_t *arena, char *content,
+                                size_t content_len, char *path, char *module) {
+    nmspc_link_t *gnt = replacer_init_nmspc_link(arena, NULL, path, "", module);
+
+    gnt->self->content = content;
+    gnt->self->content_len = content_len;
+
+    return gnt;
 }
