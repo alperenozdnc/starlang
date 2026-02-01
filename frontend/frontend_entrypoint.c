88247079cf14ff0e7103c15671dea2bb734c3de2
alperenozdnc <alperen@ozdinc.org>
2026-02-01 03:07:24 +0300

    chore(frontend): intermediate arena for holding source info

--- frontend/frontend_entrypoint.c
+++ frontend/frontend_entrypoint.c
@@ -10,15 +10,18 @@ bool frontend_entrypoint(char *rel_main_module_path, char *parent_path,
     FILE *file = fopen(rel_main_module_path, "r");
     size_t file_size = util_get_file_size(file);
 
-    arena_t *frontend_arena = arena_init(file_size);
-    char *buf = util_read_file_into_arena(frontend_arena, file);
+    arena_t *intermediate_arena = arena_init(file_size);
+    char *buf = util_read_file_into_arena(intermediate_arena, file);
 
     char *main_module_path =
-        util_build_str(frontend_arena, parent_path, filename);
+        util_build_str(intermediate_arena, parent_path, filename);
 
-    replacer(main_module_path, parent_path, filename, buf, file_size - 1);
+    src_t *source = replacer(intermediate_arena, main_module_path, parent_path,
+                             filename, buf, file_size - 1);
 
-    arena_free(frontend_arena);
+    (void)source;
+
+    arena_free(intermediate_arena);
     fclose(file);
 
     return true;
