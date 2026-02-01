b8fba84bb5aa16bf139d534f7d0920b7daa0b3ea
alperenozdnc <alperen@ozdinc.org>
2026-02-01 16:43:28 +0300

    feat(lexer): `lexeme_t` and `lexer_region_t` types

--- include/starlang/lexer.h
+++ include/starlang/lexer.h
@@ -26,6 +26,29 @@ static inline char *lexeme_to_str(lexeme_type_t t) {
     return table[t];
 }
 
+typedef struct lexeme_t {
+    lexeme_type_t type;
+
+    char *view;
+    size_t view_len;
+
+    size_t line;
+    size_t col;
+
+    struct lexeme_t *next;
+} lexeme_t;
+
+typedef struct lexer_region_t {
+    size_t start;
+    size_t end;
+
+    lexeme_t *lexemes;
+
+    char *filename;
+
+    struct lexer_region_t *next;
+} lexer_region_t;
+
 /*
  * the lexer routine - this step in the pipeline converts all non-whitespace
  * tokens into 'lexemes'. you can think about lexemes as all tokens generalized
