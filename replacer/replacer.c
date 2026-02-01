deed79f45630e88fcf55cfbcb2c7bf1658444217
alperenozdnc <alperen@ozdinc.org>
2026-02-01 03:06:57 +0300

    feat(replacer): replacer now can free its arena and pass itself to the lexer

--- replacer/replacer.c
+++ replacer/replacer.c
@@ -1,25 +1,27 @@
 #include <starlang/arena.h>
 #include <starlang/replacer.h>
 
-void replacer(char *main_module_path, char *parent_path, char *filename,
-              char *content, size_t len) {
+src_t *replacer(arena_t *intermediate_arena, char *main_module_path,
+                char *parent_path, char *filename, char *content, size_t len) {
     arena_t *replacer_arena = arena_init(len);
 
     nmspc_decl_t **decl = replacer_get_nmspc_decl(replacer_arena, parent_path);
-    nmspc_link_t *gnt =
-        replacer_init_gnt(replacer_arena, main_module_path, filename);
+    nmspc_link_t *gnt = replacer_init_gnt(replacer_arena, content, len,
+                                          main_module_path, filename);
 
     size_t link_count = 1; // the root counts too
 
     replacer_compile_gnt(replacer_arena, parent_path, decl, gnt, gnt, content,
                          len, &link_count);
 
-    replacer_visualize_gnt(gnt);
-
     nmspc_link_t **gnt_flat =
         replacer_flatten_gnt(replacer_arena, gnt, link_count);
 
-    replacer_visualize_flat_gnt(gnt_flat, link_count);
+    src_t *src = arena_alloc(intermediate_arena, sizeof(*src));
+
+    replacer_compile_src(src, intermediate_arena, gnt_flat, link_count);
 
     arena_free(replacer_arena);
+
+    return src;
 }
