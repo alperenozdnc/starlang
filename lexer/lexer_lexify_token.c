e2373f416f299b99b4cf1aba01c82251baeadbf6
alperenozdnc <alperen@ozdinc.org>
2026-02-08 18:11:28 +0300

    refactor(lexer): memset all lexemes and include line view

--- lexer/lexer_lexify_token.c
+++ lexer/lexer_lexify_token.c
@@ -13,14 +13,14 @@ static inline lexeme_t *alloc_lexeme_to_region(lexer_t *l) {
             arena_alloc(l->arena, sizeof(*l->region->lexemes));
 
         l->region->tail_lexeme = l->region->tail_lexeme->next;
-
-        memset(l->region->tail_lexeme, 0, sizeof(*l->region->tail_lexeme));
     }
+
+    memset(l->region->tail_lexeme, 0, sizeof(*l->region->tail_lexeme));
 
     return l->region->tail_lexeme;
 }
 
-void lexer_lexify_token(lexer_t *l, lexeme_type_t type, size_t len) {
+lexeme_t *lexer_lexify_token(lexer_t *l, lexeme_type_t type, size_t len) {
     lexeme_t *lex = alloc_lexeme_to_region(l);
 
     lex->line = l->line;
@@ -30,4 +30,8 @@ void lexer_lexify_token(lexer_t *l, lexeme_type_t type, size_t len) {
     lex->view_len = len;
 
     lex->type = type;
+    lex->line_view = l->line_view;
+    lex->line_view_len = l->line_len;
+
+    return lex;
 }
