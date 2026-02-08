6a77fcca2c0ce3057ecf2c8f4adc7467e4d8b9b1
alperenozdnc <alperen@ozdinc.org>
2026-02-09 00:23:51 +0300

    feat(lexer): `lexer_flatten_lexemes()` prepares info for parser

---
+++ lexer/lexer_flatten_lexemes.c
@@ -0,0 +1,48 @@
+#include <starlang/arena.h>
+#include <starlang/lexer.h>
+#include <starlang/transitions.h>
+#include <starlang/utils.h>
+
+lexical_info_t *lexer_flatten_lexemes(arena_t *trans_arena, lexer_t *l) {
+    lexical_info_t *info = arena_alloc(trans_arena, sizeof(*info));
+
+    info->lexeme_count = 0;
+
+    lexer_region_t *reg = l->base_region;
+
+    while (reg != NULL) {
+        lexeme_t *lex = reg->lexemes;
+
+        while (lex != NULL) {
+            info->lexeme_count++;
+
+            lex = lex->next;
+        }
+
+        reg = reg->next;
+    }
+
+    info->lexemes =
+        arena_alloc(trans_arena, info->lexeme_count * (sizeof(uintptr_t)));
+
+    reg = l->base_region;
+
+    size_t i = 0;
+
+    while (reg != NULL) {
+        lexeme_t *lex = reg->lexemes;
+
+        while (lex != NULL) {
+            lex_t *_lex = arena_alloc(trans_arena, sizeof(*lex));
+            memcpy(_lex, lex, sizeof(*lex) - 8);
+
+            info->lexemes[i++] = _lex;
+
+            lex = lex->next;
+        }
+
+        reg = reg->next;
+    }
+
+    return info;
+}
