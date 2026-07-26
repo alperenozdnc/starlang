65de2346878eb42201ef74d34a1a37e961c1f50d
alperenozdnc <alperenozdincc@gmail.com>
2026-07-26 15:17:00 +0300

    chore(auditer): add explanation for no data structure

--- include/starlang/auditer.h
+++ include/starlang/auditer.h
@@ -4,6 +4,10 @@
 
 #include <stdbool.h>
 
+/* the auditer is a no-modify single pass 'checker', and that's why it has
+ no inherent data structure (like lexer_t for the lexer), it only reads
+ the lexical info and either allows or stops compilation */
+
 /*
  * audits given lexical information for correct structure and obvious mistakes,
  * to decide if given lexemes are suitable to go on to be parsed. although this
